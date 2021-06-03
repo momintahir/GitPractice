f4f9a4b7be43caa26b4800d850db5ec9eb7514aa
Momin Tahir <momintahir7@gmail.com>
2021-06-03 23:40:05 +0500

    Revert "kuch bhi commmit"
    This reverts commit b3512df9

--- app/src/main/java/com/example/gitpractice/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("this is my second commmit")
-        kuch bhi
+        ye error geenerate hoga
     }
 }
