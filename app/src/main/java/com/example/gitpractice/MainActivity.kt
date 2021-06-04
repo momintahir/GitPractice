f40b18aa46ce7c075908ba1779703553bcf5e8dd
Momin Tahir <momintahir7@gmail.com>
2021-06-05 02:28:21 +0500

    change 1 commit

--- app/src/main/java/com/example/gitpractice/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("this is my second commmit")
-        println("this commit is in experimental")
+        println("Change 1")
     }
 }
